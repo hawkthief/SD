faa09bed6a32ac8d18ffbd8b6e097794cc2bcd3d
hawkthief <daniellbl@hotmail.com>
2022-07-06 19:59:58 -0300

    adminLTE

--- TP_SD/artigos/models.py
+++ TP_SD/artigos/models.py
@@ -5,5 +5,4 @@ from django.db import models
 
 class art(models.Model):
     titulo = models.CharField(verbose_name="título",max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+    pub_date = models.DateTimeField('data de publicação')
