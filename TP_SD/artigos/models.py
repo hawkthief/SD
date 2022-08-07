12e0614356acf3fb104158ecf1a80e1d7c84db2c
hawkthief <daniellbl@hotmail.com>
2022-08-07 18:36:21 -0300

    db

--- TP_SD/artigos/models.py
+++ TP_SD/artigos/models.py
@@ -1,23 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 from datetime import date
 
 class art(models.Model):
     titulo = models.CharField(verbose_name="título",max_length=200)
     subtitulo = models.CharField(verbose_name="subtítulo",max_length=200)
     pub_date = models.DateTimeField('data de publicação')
-#Transformar autor em Objeto ID
-#autor = models.CharField(verbose_name="autor",max_length=100)
+    autor = models.ForeignKey('usuario', on_delete=models.CASCADE)
     palavra_chave = models.CharField(verbose_name="palavra_chave", max_length=500)
 
     def __str__(self):
         return self.name
 
-class usuario(models.Model):
+
+class usuario(AbstractUser):
 
     name = models.CharField(verbose_name="name",max_length=100)
     email = models.EmailField('email')
-    funcao = models.CharField(verbose_name="função",max_length=100)
+    role = models.CharField(verbose_name="função",max_length=100)
 
     def __str__(self):
         return self.name
