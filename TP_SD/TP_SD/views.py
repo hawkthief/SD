a25f910fb04531d63f063019ad13118d7cac3d79
hawkthief <daniellbl@hotmail.com>
2022-08-09 16:20:32 -0300

    SearchForm pt2

--- TP_SD/TP_SD/views.py
+++ TP_SD/TP_SD/views.py
@@ -4,20 +4,10 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from .forms import newuserForm
-from artigos.forms import searchForm
-from artigos.models import *
-import pandas as pd
 
 @login_required()
 def home(request):
-    if request.method == 'POST':
-        form = searchForm(request.POST)
-        if form.is_valid():
-            pass
-        return redirect('/artigos/')
-    else:
-        form = searchForm()
-    return render(request, 'index.html', {'form': form})
+    return redirect('/artigos/search')
 
 @login_required()
 def manageusers(request):
